b2a34def92612c5ee071e892355322dc67aca8a2
M'barek Manouze <mmanouze@e1r4p11.1337.ma>
2022-08-30 12:43:52 +0100

    no pipes leaks

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmanouze <mmanouze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 12:52:39 by ressalhi          #+#    #+#             */
-/*   Updated: 2022/08/25 16:22:50 by mmanouze         ###   ########.fr       */
+/*   Updated: 2022/08/30 12:42:12 by mmanouze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ void	ft_free(t_parse *parse)
 	i = 0;
 	while (i < parse->num_data)
 	{
+		close(parse->data[i].fd[0]);
 		free(parse->data[i].cmd);
 		j = 0;
 		while (parse->data[i].args[j])
